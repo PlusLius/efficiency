421c0c4dd127325905d6051fc023d4596504667f
LiuPlus <408709581@qq.com>
2018-03-11 21:25:00 +0800

    system

---
+++ js/MvvM框架(mvvmFrameWork)/MVVM/index.js
@@ -0,0 +1,30 @@
+function Vue(options){
+    var self = this;
+    this.data = options.data;
+    this.methods = options.methods;
+
+    Object.keys(this.data).forEach(function(key){
+        self.proxyKeys(key)
+    });
+
+    observe(this.data)
+    new Compile(options.el,this)
+    options.mounted.call(this) //所以事情执行好执行mounted函数
+
+}
+
+Vue.prototype = {
+    proxyKeys:function(key){
+        var self = this;
+        Object.defineProperty(this,key,{
+            enumerable:false,
+            configurable:true,
+            get:function(){
+                return self.data[key]
+            },
+            set:function(newVal){
+                self.data[key] = newVal;
+            }
+        })
+    }
+}
